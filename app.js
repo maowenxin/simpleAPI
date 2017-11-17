2eaf8aa3f3f9d78d40324dad7e13e08121b540be
Wen Mao <wenxin.mao@jpmchase.com>
2017-11-17 15:09:26 -0600

    return json

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/people', function (req, res) {
+	let people = {"people":[{ "id": 1, "firstName":"f","lastName":"l"}]};
+  res.json(people);
 })
 
 app.listen(3000, function () {
